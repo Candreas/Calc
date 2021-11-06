ec095e3d44484bccd11437a7739c2dee2b3af861
Candreas <lindenthal100@gmail.com>
2021-11-06 10:32:26 +0100

    Update CalculatorHistory.cpp

--- src/CalcManager/CalculatorHistory.cpp
+++ src/CalcManager/CalculatorHistory.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.jj ll
+// Licensed under the MIT License.jj fff
 
 #include <cassert>
 #include "CalculatorHistory.h"
